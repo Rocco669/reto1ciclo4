1fafb3a73b0181dd0cbf9589a71cc3daa0a304ee
Ronald Gomez Poveda <91928804+Rocco669@users.noreply.github.com>
2021-11-24 18:29:26 -0500

    Add files via upload
    reto1ciclo4

---
+++ src/main/java/Reto1/UserCrudRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto1;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author rocco
+ */
+public interface UserCrudRepository extends CrudRepository <User, Integer> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email,String password);
+    
+}
